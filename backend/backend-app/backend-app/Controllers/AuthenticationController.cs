42a30620f8162095e85bb2d01b7232d0752a77cc
luyando hambala <luyandohambala.1@gmail.com>
2026-03-22 10:41:07 +0200

    backend files and sample code

---
+++ backend/backend-app/backend-app/Controllers/AuthenticationController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using smartstock_inventory_service.Services;
+
+namespace backend_app.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthenticationController(AuthenticationService authenticationService) : ControllerBase
+    {
+        [HttpGet("name")] //http method type is passed here. In hte paranthesis, pass the route i.e ("route/you/want")
+        public async Task<ActionResult> GetName()
+        {
+            /*
+             * use method aliases like OK to inform the http server to return an http 200
+             if you need dto validation, consider the following model state validation
+             
+             if (!ModelState.IsValid) return BadRequest(ModelState);
+             */
+            return Ok(await authenticationService.GetName());
+        }
+    }
+}
---
+++ backend/backend-app/backend-app/Services/AuthenticationService.cs
@@ -0,0 +1,12 @@
+﻿namespace smartstock_inventory_service.Services
+{
+    public partial class AuthenticationService
+    {
+
+        //all logic should live in the service and only be called from the controller
+        public async Task<string> GetName()
+        {
+            return await Task.Run(() => "luyando");
+        }
+    }
+}
